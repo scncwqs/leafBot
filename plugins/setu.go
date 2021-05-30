19ef181435cd969450c803d23ebd9962b25475af
尐酱油 <scncwqs@126.com>
2021-05-30 23:31:06 +0800

    Update setu.go

--- plugins/setu.go
+++ plugins/setu.go
@@ -7,7 +7,7 @@ import (
 )
 
 func UseSetuHandle() {
-	leafBot.OnCommand("/setu").
+	leafBot.OnCommand("/tu").
 		AddAllies("来点色图").
 		SetWeight(10).
 		SetBlock(false).
@@ -15,13 +15,13 @@ func UseSetuHandle() {
 		AddHandle(
 			func(event leafBot.Event, bot *leafBot.Bot, args []string) {
 				if len(args) < 1 {
-					bot.SendMsg(event.MessageType, event.UserId, event.GroupId, message.ParseMessageFromString(cqCode.Image("https://acg.toubiec.cn/random.php", map[string]interface{}{"cache": 0, "c": 3})))
-				} else if args[0] == "r18" {
-					bot.SendMsg(event.MessageType, event.UserId, event.GroupId, message.ParseMessageFromString(cqCode.Image("https://api.pixivweb.com/anime18r.php?return=img", map[string]interface{}{"cache": 0, "c": 3})))
-				} else if args[0] == "true" {
-					bot.SendMsg(event.MessageType, event.UserId, event.GroupId, message.ParseMessageFromString(cqCode.Image("https://api.pixivweb.com/api.php?return=img/json", map[string]interface{}{"cache": 0, "c": 3})))
-				} else if args[0] == "r18+true" {
-					bot.SendMsg(event.MessageType, event.UserId, event.GroupId, message.ParseMessageFromString(cqCode.Image("https://api.pixivweb.com/bw.php?return=img", map[string]interface{}{"cache": 0, "c": 3})))
+					bot.SendMsg(event.MessageType, event.UserId, event.GroupId, message.ParseMessageFromString(cqCode.Image("https://laosepi.org/pic.php", map[string]interface{}{"cache": 0, "c": 3})))
+				} else if args[0] == "mc" {
+					bot.SendMsg(event.MessageType, event.UserId, event.GroupId, message.ParseMessageFromString(cqCode.Image("https://laosepi.org/mcpic.php", map[string]interface{}{"cache": 0, "c": 3})))
+				} else if args[0] == "sj" {
+					bot.SendMsg(event.MessageType, event.UserId, event.GroupId, message.ParseMessageFromString(cqCode.Image("https://laosepi.org/sjpic.php", map[string]interface{}{"cache": 0, "c": 3})))
+				} else if args[0] == "gq" {
+					bot.SendMsg(event.MessageType, event.UserId, event.GroupId, message.ParseMessageFromString(cqCode.Image("https://laosepi.org/gqpic.php", map[string]interface{}{"cache": 0, "c": 3})))
 				}
 			})
 
